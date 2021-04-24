1126f9fe4a5cd18b5b227d42dd637790af3283ae
Yutaro Konda <kondayutarou@gmail.com>
2021-04-25 07:48:08 +0900

    Add undo function

--- app/src/main/java/com/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/shoppinglist/MainActivity.kt
@@ -3,12 +3,14 @@ package com.shoppinglist
 import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.jakewharton.rxrelay3.BehaviorRelay
 import com.shoppinglist.databinding.ActivityMainBinding
 import com.shoppinglist.databinding.DialogueAddBinding
@@ -58,6 +60,8 @@ class MainActivity : AppCompatActivity() {
             object : ItemTouchHelper.SimpleCallback(
                 ItemTouchHelper.LEFT, ItemTouchHelper.RIGHT
             ) {
+                private var deletedItem: ShoppingListItem? = null
+                private var deletedPosition: Int? = null
                 override fun onMove(
                     recyclerView: RecyclerView,
                     viewHolder: RecyclerView.ViewHolder,
@@ -70,13 +74,32 @@ class MainActivity : AppCompatActivity() {
                     if (direction == ItemTouchHelper.RIGHT) {
                         val position = viewHolder.adapterPosition
                         val value = shoppingList.value
-                        value.remove(value[position])
+                        deletedPosition = position
+                        deletedItem = value[position]
+                        value.remove(deletedItem)
                         shoppingList.accept(value)
                         recyclerView.adapter?.notifyItemRemoved(position)
+                        showSnackBar()
                     }
                 }
-            })
 
+                private fun showSnackBar() {
+                    Snackbar.make(
+                        recyclerView,
+                        "${deletedItem?.name as CharSequence} ${resources.getString(R.string.removed)}",
+                        Snackbar.LENGTH_LONG
+                    )
+                        .setAction(R.string.undo, View.OnClickListener {
+                            val value = shoppingList.value
+                            val position = deletedPosition ?: return@OnClickListener
+                            val item = deletedItem ?: return@OnClickListener
+                            value.add(position, item)
+                            shoppingList.accept(value)
+                            recyclerView.adapter?.notifyItemInserted(position)
+                        })
+                        .show()
+                }
+            })
     }
 
     private fun buildDialogue(activity: Activity): AlertDialog {
