21af1f513ea083f5d7b3c2b37f77bfee3c05418d
Yutaro Konda <kondayutarou@gmail.com>
2021-09-28 06:58:43 +0900

    Add OkHttp and Retrofit

--- app/src/main/java/com/lecture_list/application/ApplicationModule.kt
+++ app/src/main/java/com/lecture_list/application/ApplicationModule.kt
@@ -2,6 +2,7 @@ package com.lecture_list.application
 
 import android.content.Context
 import androidx.room.Room
+import com.lecture_list.BuildConfig
 import com.lecture_list.data.source.local.AppDatabase
 import com.lecture_list.view.MainViewModel
 import dagger.Module
@@ -9,7 +10,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import okhttp3.OkHttpClient
 import javax.inject.Singleton
+import okhttp3.logging.HttpLoggingInterceptor
 
 @Module
 @InstallIn(ActivityComponent::class)
@@ -26,5 +29,23 @@ class ApplicationModule {
     @Provides
     fun provideMainViewModel(@ApplicationContext context: Context): MainViewModel {
         return MainViewModel(provideDb(context))
+    }
+
+    @Singleton
+    fun provideOkHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val httpLogger = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            httpLogger.setLevel(HttpLoggingInterceptor.Level.BASIC)
+        } else {
+            httpLogger.setLevel(HttpLoggingInterceptor.Level.NONE)
+        }
+        return httpLogger
+    }
+
+    @Singleton
+    fun provideOkHttpClient(httpLogger: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLogger)
+            .build()
     }
 }
