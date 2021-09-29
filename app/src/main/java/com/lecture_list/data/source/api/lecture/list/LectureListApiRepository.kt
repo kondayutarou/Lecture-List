667194096fd62726b6ed049297c3872faf239567
Yutaro Konda <kondayutarou@gmail.com>
2021-09-29 21:46:59 +0900

    Fix observing thread to io

--- app/src/main/java/com/lecture_list/data/source/api/lecture/list/LectureListApiRepository.kt
+++ app/src/main/java/com/lecture_list/data/source/api/lecture/list/LectureListApiRepository.kt
@@ -2,6 +2,7 @@ package com.lecture_list.data.source.api.lecture.list
 
 import com.lecture_list.model.LectureListItemApi
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import retrofit2.Retrofit
 import javax.inject.Inject
 
@@ -11,5 +12,6 @@ class LectureListApiRepository @Inject constructor(
     override fun fetchLectureListObservable(): Observable<List<LectureListItemApi>> {
         return retrofit.create(LectureListApiInterface::class.java)
             .fetchLectureList()
+            .subscribeOn(Schedulers.io())
     }
 }
