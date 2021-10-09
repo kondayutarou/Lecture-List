481db754199d31e6ecc7c6e5fabf042470521b80
Yutaro Konda <kondayutarou@gmail.com>
2021-10-09 13:07:00 +0900

    Populate ViewModel

--- app/src/main/java/com/lecture_list/view/MainViewModel.kt
+++ app/src/main/java/com/lecture_list/view/MainViewModel.kt
@@ -1,13 +1,23 @@
 package com.lecture_list.view
 
 import androidx.lifecycle.ViewModel
+import com.jakewharton.rxrelay3.BehaviorRelay
 import com.lecture_list.data.LectureListRepository
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
     private val repository: LectureListRepository
 ) : ViewModel() {
-    fun start() {
+    private val dataLoading = BehaviorRelay.create<Boolean>()
+    private var dataLoaded = false
 
+    fun start() {
+        if (dataLoading.value == true) {
+            return
+        } else {
+            dataLoading.accept(true)
+        }
+
+        repository.getLectureList()
     }
 }
